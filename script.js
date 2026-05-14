3f9eaf49b730a3f475e7117d3407ed3964f4ff26
saruganesan7-ui <saruganesan7@gmail.com>
2026-05-14 19:38:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showScore(){
+
+document.body.innerHTML += `
+
+<h2>Interview Readiness Score: 80/100</h2>
+
+<p>Suggestions:</p>
+
+<ul>
+<li>Improve communication skills</li>
+<li>Add more projects</li>
+</ul>
+
+`;
+
+}
